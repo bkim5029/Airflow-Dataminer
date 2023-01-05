0c6689b1f3b93a2ea53e35c4b83e3e7145d49aac
bkim5029 <50348130+bkim5029@users.noreply.github.com>
2023-01-05 04:38:37 -0500

    Update insert_data.sh

--- insert_data.sh
+++ insert_data.sh
@@ -2,9 +2,9 @@
 save_file_location=$1
 options_table_name=$2
 
-echo jys502943 | sudo -S sudo service mysql start
+echo jys502943 | sudo -S sudo service mysql start # Starts mysql server and enters in the sudo password
 
-mysql -uroot -pJys502943! options_database --local_infile=1 <<EOF #opens mysql server and enters the commands written in EOF
+mysql -uroot -pJys502943! options_database --local_infile=1 <<EOF # Opens mysql database and enters the commands written in EOF
 
 SET GLOBAL local_infile=True;
 
